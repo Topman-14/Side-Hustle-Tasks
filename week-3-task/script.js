0fa39b52dd4e401225e5274e8bd1ca43d6615f44
Topman-14 <talktotimothy101@gmail.com>
2023-03-26 17:47:16 +0100

    add menu functionality, snap scrol to elements, fix bugs

--- week-3-task/script.js
+++ week-3-task/script.js
@@ -1,1 +1,31 @@
-console.log('hello world')
+const menuBtn = document.querySelector('#menu');
+const mobileMenu = document.querySelector('#mobile-nav');
+const menuBack = document.querySelector("#back-button");
+const mobileNavLink = document.querySelectorAll('.nav-link-mobile')
+
+
+menuBtn.addEventListener('click', () =>{
+    mobileMenu.style.animation ='slide-left 0.7s';
+
+    setTimeout(()=>{
+        mobileMenu.style.display = 'unset'
+    }, 50)
+})
+
+menuBack.addEventListener('click', () =>{
+    mobileMenu.style.animation ='slide-right 0.7s';
+
+    setTimeout(()=>{
+        mobileMenu.style.display = 'none'
+    }, 650)
+})
+
+mobileNavLink.forEach(link => {
+    link.addEventListener('click', () =>{
+        mobileMenu.style.animation ='slide-right 0.7s';
+    
+        setTimeout(()=>{
+            mobileMenu.style.display = 'none'
+        }, 650)
+    })
+});
